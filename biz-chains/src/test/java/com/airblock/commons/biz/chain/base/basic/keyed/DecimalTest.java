15a7f3840f97e8603f6f1b94475137d206c3d8ed
linyitian1 <linyitian1@jd.com>
2022-12-14 19:02:04 +0800

    solve generic problem

--- biz-chains/src/test/java/com/airblock/commons/biz/chain/base/basic/keyed/DecimalTest.java
+++ biz-chains/src/test/java/com/airblock/commons/biz/chain/base/basic/keyed/DecimalTest.java
@@ -3,6 +3,9 @@ package com.airblock.commons.biz.chain.base.basic.keyed;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.regex.Pattern;
 
 public class DecimalTest {
 
@@ -12,6 +15,18 @@ public class DecimalTest {
         System.out.println(bigDecimal.scale());
         bigDecimal.setScale(10);
         System.out.println(bigDecimal.precision());
+    }
+
+    @Test
+    public void test2() {
+        double d = 123456789.1000;
+        String format = "^([1-9]\\d*|0)(\\.\\d{1,3})?$";
+        boolean matches = Pattern.matches(format, "0.1000");
+        System.out.println(matches);
+        BigDecimal bigDecimal = (new BigDecimal(d)).setScale(3, RoundingMode.HALF_UP);
+        String x = bigDecimal.toString();
+        System.out.println(x);
+        System.out.println(Pattern.matches(format, x));
     }
 
 }
